f9c9b75603f3d1daa9daed49ff65527c99a40b8c
komesh26 <38267602+komesh26@users.noreply.github.com>
2023-11-06 20:13:45 +1300

    Feat: Initial (#2)

--- index.js
+++ index.js
@@ -3,25 +3,42 @@
 
 // init project
 require('dotenv').config();
-var express = require('express');
+let bodyParser = require("body-parser");
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require('cors');
+var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+// your first API endpoint...
+app.get("/api/whoami", function (req, res) {
+  console.log("req", req.headers);
+  var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  var lang = req.headers["accept-language"];
+  var software = req.headers["user-agent"];
+
+  console.log("reeee", ip, lang, software);
+  //   {"ipaddress": req.ip ,"language":"en-US,en;q=0.5",
+  // "software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"}
+  res.json({ ipaddress: ip, language: lang, software: software });
 });
 
 // listen for requests :)
